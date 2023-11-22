29e006b8c6449610863d7d4cf4ae5992b8d7c5f9
Aaron <caper2711@hotmail.com>
2023-11-22 13:50:55 -0600

    Main limpio

--- FarmaManage.cpp
+++ FarmaManage.cpp
@@ -1,11 +1,7 @@
 #include "UI/mainUI.h"
-#include <clocale>
 
 int main(){
-    setlocale(LC_CTYPE,"Spanish");
     MainUI ui;
     ui.run();
-    cout<<"         .:Vuelva pronto:.\n";
-    system("pause");
     return 0;
 }
